4e18ee577c06486449e5a30cddce9f35756bb605
To_Craft <development@tocraft.dev>
2024-11-13 16:30:41 +0100

    fix color codecs

--- common/src/main/java/dev/tocraft/ctgen/util/Codecs.java
+++ common/src/main/java/dev/tocraft/ctgen/util/Codecs.java
@@ -8,15 +8,16 @@ import net.minecraft.world.level.block.Block;
 
 public final class Codecs {
     public static final Codec<Integer> COLOR_RGB = RecordCodecBuilder.create(instance -> instance.group(
-            Codec.INT.fieldOf("r").forGetter(i -> (i >> 16) & 0xFF),
-            Codec.INT.fieldOf("g").forGetter(i -> (i >> 8) & 0xFF),
-            Codec.INT.fieldOf("b").forGetter(i -> i & 0xFF)
+            Codec.INT.fieldOf("red").forGetter(i -> (i >> 16) & 0xFF),
+            Codec.INT.fieldOf("green").forGetter(i -> (i >> 8) & 0xFF),
+            Codec.INT.fieldOf("blue").forGetter(i -> i & 0xFF)
     ).apply(instance, instance.stable((r, g, b) -> (0xFF << 24) |
             ((r & 0xFF) << 16) |
             ((g & 0xFF) << 8)  |
             ((b & 0xFF)))));
 
-    public static final Codec<Integer> COLOR_HEX = Codec.STRING.xmap(Integer::decode, i -> String.format("#%02x%02x%02x", (i >> 16) & 0xFF, (i >> 8) & 0xFF, i & 0xFF));
+    public static final Codec<Integer> COLOR_HEX = Codec.STRING.xmap(s -> ((0xFF) << 24) |
+            Integer.decode(s), i -> String.format("#%02x%02x%02x", (i >> 16) & 0xFF, (i >> 8) & 0xFF, i & 0xFF));
 
     public static final Codec<Integer> COLOR = Codec.either(COLOR_HEX, COLOR_RGB)
             .xmap(either -> either.left().orElseGet(() -> either.right().orElseThrow()), Either::left);
